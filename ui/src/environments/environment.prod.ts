f17d6e1ee4f549d10d82bc4847286a444ff12adb
galwort <wthomasgorbett@gmail.com>
2023-10-18 20:45:54 -0400

    ng add @angular/fire

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDm7iyVJA_NfDmxirWSIz-4zaISlEo_Z3A",
-    authDomain: "paper-122c4.firebaseapp.com",
-    projectId: "paper-122c4",
-    storageBucket: "paper-122c4.appspot.com",
-    messagingSenderId: "336450249602",
-    appId: "1:336450249602:web:f9196c4ddf1938b5b4a59b",
-    measurementId: "G-PS3LE6RHMS"
+    projectId: 'paper-122c4',
+    appId: '1:336450249602:web:f9196c4ddf1938b5b4a59b',
+    storageBucket: 'paper-122c4.appspot.com',
+    apiKey: 'AIzaSyDm7iyVJA_NfDmxirWSIz-4zaISlEo_Z3A',
+    authDomain: 'paper-122c4.firebaseapp.com',
+    messagingSenderId: '336450249602',
+    measurementId: 'G-PS3LE6RHMS',
   }
 };
