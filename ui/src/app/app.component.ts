2557d36603dcb5ec360e2bc1a11134539b083fef
galwort <wthomasgorbett@gmail.com>
2023-10-23 07:40:48 -0400

    Create sidebar

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  public profilePicUrl: string;
+  isHomePage: boolean = false;
+  isLoginOrProfilePage: boolean = false;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.authService.userPic.subscribe(url => {
+      this.profilePicUrl = url;
+    });
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomePage = event.urlAfterRedirects === '/home';
+        this.isLoginOrProfilePage = (event.urlAfterRedirects === '/login') || (event.urlAfterRedirects === '/profile');
+      }
+    });
+  }
+
+  navigateToGitHub() {
+    window.open('https://github.com/galwort/paper', '_blank');
+  }
+
+  loginOrProfile() {
+    const userId = this.authService.getCurrentUserId();
+    if (userId) {
+      this.router.navigate(['/profile']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }  
 }
