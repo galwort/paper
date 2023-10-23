19c8ff11ce81fa7fcc8ee8269cd816dccc2181cc
galwort <wthomasgorbett@gmail.com>
2023-10-22 20:20:04 -0400

    Add google login functionality

--- ui/src/app/pages/login/login.page.ts
+++ ui/src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { LoginData } from 'src/app/interfaces/login-data.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginData: LoginData = { email: '', password: '' };
 
-  constructor() { }
-
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
   ngOnInit() {
   }
 
+  loginWithGoogle() {
+    this.authService
+      .loginWithGoogle()
+      .then(() => this.router.navigate(['']))
+      .catch((e) => console.error('Google login failed:', e.message));
+  }
 }
