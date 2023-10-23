65ba4bb465b9b0068da22f83a228dc1549e41945
galwort <wthomasgorbett@gmail.com>
2023-10-23 06:56:47 -0400

    Fix same letters moving together

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -10,30 +10,29 @@ export class HomePage implements OnInit {
   arrowStates: { [key: string]: { up: boolean, down: boolean, secondUp: boolean, secondDown: boolean }} = {};
 
   ngOnInit() {
-    ['P', 'A', 'P', 'E', 'R'].forEach(letter => {
-      this.arrowStates[letter] = { up: false, down: false, secondUp: false, secondDown: false };
+    ['P', 'A', 'P', 'E', 'R'].forEach((letter, index) => {
+      this.arrowStates[index] = { up: false, down: false, secondUp: false, secondDown: false };
     });
   }
-
-  changePrimaryColor(event: Event, letter: string, direction: 'up' | 'down') {
+  changePrimaryColor(event: Event, index: number, direction: 'up' | 'down') {
     const clickedClass = direction === 'up' ? 'clicked-up' : 'clicked-down';
     this.toggleClass(event, clickedClass);
-    this.arrowStates[letter][direction] = !this.arrowStates[letter][direction];
+    this.arrowStates[index][direction] = !this.arrowStates[index][direction];
   }
 
-  changeSecondaryColor(event: Event, letter: string, direction: 'up' | 'down') {
+  changeSecondaryColor(event: Event, index: number, direction: 'up' | 'down') {
     const clickedClass = direction === 'up' ? 'clicked-up' : 'clicked-down';
-    const secondaryDirection = `second${direction.charAt(0).toUpperCase() + direction.slice(1)}` as keyof typeof this.arrowStates[typeof letter];
+    const secondaryDirection = `second${direction.charAt(0).toUpperCase() + direction.slice(1)}` as keyof typeof this.arrowStates[typeof index];
     this.toggleClass(event, clickedClass);
-    this.arrowStates[letter][secondaryDirection] = !this.arrowStates[letter][secondaryDirection];
+    this.arrowStates[index][secondaryDirection] = !this.arrowStates[index][secondaryDirection];
   }
 
-  showSecondUpArrow(letter: string): boolean {
-    return this.arrowStates[letter]?.up || false;
+  showSecondUpArrow(index: number): boolean {
+    return this.arrowStates[index]?.up || false;
   }
 
-  showSecondDownArrow(letter: string): boolean {
-    return this.arrowStates[letter]?.down || false;
+  showSecondDownArrow(index: number): boolean {
+    return this.arrowStates[index]?.down || false;
   }
 
   private toggleClass(event: Event, className: string) {
