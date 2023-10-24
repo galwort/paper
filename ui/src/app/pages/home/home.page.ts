78e32ed631eba1b7996b12c90d21cf421f275d2e
galwort <wthomasgorbett@gmail.com>
2023-10-24 15:01:19 -0400

    Write to firebase on submit

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { getFirestore, doc, collection, setDoc, Timestamp } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { environment } from 'src/environments/environment';
+
+export const app = initializeApp(environment.firebase);
+export const db = getFirestore(app);
 
 @Component({
   selector: 'app-home',
@@ -7,7 +14,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  constructor(
+      private authService: AuthService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  async submit() {
+    const userId = this.authService.getCurrentUserId();
+
+    const userRankingRef = doc(collection(db, 'papers'));
+    await setDoc(userRankingRef, {
+      user: userId,
+      timestamp: Timestamp.now(),
+    });
   }
 }
